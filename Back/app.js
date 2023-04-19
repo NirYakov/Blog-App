feebba16ababea0ba59fc2c0dfb8626f88e1352e
Nir y <Niry27@gmail.com>
2023-04-19 16:01:31 +0300

    Connect to mongoDB

--- Back/app.js
+++ Back/app.js
@@ -1,7 +1,69 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const Post = require('./models/post');
 
 const app = express();
+
+const fs = require("fs");
+
+
+function connectToMyMongo() {
+    //////// read the username and the password from my file.
+    console.log("Myyy Data");
+
+    fs.readFile("./../../MyMongoDb.json", "utf8", (err, jsonString) => {
+        if (err) {
+            console.log("Error reading file from disk:", err);
+            return;
+        }
+        try {
+            const userDB = JSON.parse(jsonString);
+            console.log("userDB username is:", userDB.username);
+            console.log("userDB password is:", userDB.password);
+
+            const uri = `mongodb+srv://Nir:${userDB.password}@cluster0.tcpdrjy.mongodb.net/Testt?retryWrites=true&w=majority`;
+
+            //            mongoDb connect
+            // mongoose.connect(`mongodb+srv://Nir:${userDB.password}@cluster0.tcpdrjy.mongodb.net/?retryWrites=true&w=majority`).then(() => {
+            //     console.log('Connected to database!');
+            // }).catch(() => {
+            //     console.log("Connection failed!");
+            // });
+
+
+            console.log("uri :", uri);
+
+            // Prints "MongoError: bad auth Authentication failed."
+
+            mongoose.connect(uri, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                serverSelectionTimeoutMS: 5000
+            }).then(() => {
+                console.log('Connected to database');
+            }).catch(err => { console.log(err.reason); console.log("Connection failed!"); });
+
+
+            // End :  mongoDb connect
+
+
+
+
+            console.log("Success :)");
+
+        } catch (err) {
+            console.log("Error parsing JSON string:", err);
+        }
+    });
+
+    ///////
+}
+
+connectToMyMongo();
+
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,11 +82,18 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-    const post = req.body;
+    const post = new Post({
+        title: req.body.title,
+        content: req.body.content
+    });
+
     console.log(post);
+
+    post.save();
     res.status(201).json({
         message: 'Post added successfully'
     });
+
 });
 
 app.get("/api/posts", (req, res, next) => {
