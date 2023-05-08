e94a45b2109ed1075dec170276e560fd77f2e571
Nir y <Niry27@gmail.com>
2023-05-08 11:27:28 +0300

    Try more way to connet db

--- Back/MongoConnection.js
+++ Back/MongoConnection.js
@@ -1,47 +1,62 @@
 const mongoose = require("mongoose");
 const fs = require("fs");
 
+
+const connectByReadJsonFile = () => {
+
+    process.env.JWT_KEY = "secret_this_should_be_longer";
+
+    fs.readFile("./../../MyMongoDb.json", "utf8", (err, jsonString) => {
+        if (err) {
+            console.log("Error reading file from disk:", err);
+            return;
+        }
+        try {
+            const userDB = JSON.parse(jsonString);
+            console.log("userDB username is:", userDB.username);
+            console.log("userDB password is:", userDB.password);
+
+            const uri = `mongodb+srv://Nir:${userDB.password}@cluster0.tcpdrjy.mongodb.net/Testt`;
+
+            mongoose.connect(uri, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                serverSelectionTimeoutMS: 5000
+            }).then(() => {
+                console.log('Connected to database');
+            }).catch(err => { console.log(err.reason); console.log("Connection failed!"); });
+
+        } catch (err) {
+            console.log("Error parsing JSON string:", err);
+        }
+    });
+};
+
+
 module.exports = function connectToMyMongo() {
     //////// read form api key env ? !
 
-    const uri = `mongodb+srv://Nir:${process.env.MONGO_ATLAS_PW}@cluster0.tcpdrjy.mongodb.net/Testt`;
+    if (process.env.MONGO_ATLAS_PW) {
 
-    mongoose.connect(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        serverSelectionTimeoutMS: 5000
-    }).then(() => {
-        console.log('Connected to database');
-    }).catch(err => { console.log(err.reason); console.log("Connection failed!"); });
+        const uri = `mongodb+srv://Nir:${process.env.MONGO_ATLAS_PW}@cluster0.tcpdrjy.mongodb.net/Testt`;
+        mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000
+        }).then(() => {
+            console.log('Connected to database');
+        }).catch(err => {
+            console.log(err.reason);
+            console.log("Connection failed!");
+            console.log("Try one more time");
+            connectByReadJsonFile();
+        });
+
+    } else {
+
+        ////// read the username and the password from my file.
+        connectByReadJsonFile();
 
 
-
-
-
-    //////// read the username and the password from my file.
-
-    // fs.readFile("./../../MyMongoDb.json", "utf8", (err, jsonString) => {
-    //     if (err) {
-    //         console.log("Error reading file from disk:", err);
-    //         return;
-    //     }
-    //     try {
-    //         const userDB = JSON.parse(jsonString);
-    //         console.log("userDB username is:", userDB.username);
-    //         console.log("userDB password is:", userDB.password);
-
-    //         const uri = `mongodb+srv://Nir:${userDB.password}@cluster0.tcpdrjy.mongodb.net/Testt`;
-
-    //         mongoose.connect(uri, {
-    //             useNewUrlParser: true,
-    //             useUnifiedTopology: true,
-    //             serverSelectionTimeoutMS: 5000
-    //         }).then(() => {
-    //             console.log('Connected to database');
-    //         }).catch(err => { console.log(err.reason); console.log("Connection failed!"); });
-
-    //     } catch (err) {
-    //         console.log("Error parsing JSON string:", err);
-    //     }
-    // });
+    }
 }
